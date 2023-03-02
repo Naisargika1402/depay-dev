6e2746b01bbdc91c10e4b2b4d980558a7d7947fb
Abishek Upadhyay <98577878+abishek023@users.noreply.github.com>
2023-03-02 14:01:59 +0530

    Add files via upload

---
+++ components/addFunds.js
@@ -0,0 +1,75 @@
+import React, { useRef } from "react";
+import { useState, useEffect } from "react";
+import DePay from "../ethereum/DePay";
+import web3 from "../ethereum/web3";
+import classes from "./addFunds.module.css";
+import Modal from "../UI/Modal";
+import Loader from "../UI/Loader";
+const addFunds = (props) => {
+  const [accounts, setAccounts] = useState("#");
+  const [msg, setMsg] = useState({ value: "", status: 1 });
+  const [processing,setProcessing]=useState(false);
+  const amountRef = useRef();
+  useEffect(() => {
+    const load = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccounts(accounts);
+    };
+    load();
+  }, []);
+  setInterval(() => {
+    const load = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccounts(accounts);
+    };
+    load();
+  }, 5000);
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    setProcessing(true);
+    const func = async () => {
+      try {
+        await DePay.methods
+          .addFund()
+          .send({ from: accounts[0], value: amountRef.current.value });
+        amountRef.current.value = 0;
+        setMsg({value:"Success",status:1});
+        setProcessing(false);
+      } catch (err) {
+        setMsg({value:err.message,status:0});
+        setProcessing(false);
+      }
+    };
+    func();
+  };
+  const onClickHandler = () => {
+    setMsg({value:"",status:1});
+  };
+  return (
+    <Modal dontShow={props.dontShow}>
+      <h1>ADD FUNDS</h1>
+      {accounts!='#'&&<h3>Account Connected : {accounts[0]}</h3>}
+      <div className={classes.loadBox}>
+     {accounts=='#'&&<Loader></Loader>}
+     </div>
+      <form onSubmit={onSubmitHandler}>
+        {accounts!='#'&&<div className={classes.input}>
+          <label htmlFor="amount">Enter The Amount : </label>
+          <input
+            onClick={onClickHandler}
+            ref={amountRef}
+            type="number"
+            placeholder="Weis"
+          ></input>
+        </div>}
+        <div>
+          {!processing&&accounts!='#'&&<button className={classes.button}>Submit</button>}
+          {<button className={classes.button} onClick={props.dontShow}>Close</button>}
+        </div>
+        <h4 className={`${msg.status==1&&classes.success} ${msg.status==0&&classes.failure}`}>{msg.value}</h4>
+      </form>
+    </Modal>
+  );
+};
+
+export default addFunds;
---
+++ pages/UserPagecomponents/PayCard.js
@@ -0,0 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import classes from './PaymtCard.module.css';
+
+function PayCard(props) {
+  return (
+    <Card className={`mt-1 ${classes.pcards}`}>
+      <Card.Img variant="top" className={classes.logo} src="https://i.ibb.co/ftkmQvB/De-Pay-removebg-preview.png"  alt="logo" border="0" />
+      <Card.Body>
+        <div className={classes.details}>
+            <Card.Title>ADDRESS:</Card.Title>
+            <Card.Text className={classes.head}>
+                <p>{props.address}</p>
+            </Card.Text>
+        </div>
+        <hr className={classes.mar}></hr>
+        <div className={classes.details}>
+        <Card.Title>AMOUNT:</Card.Title>
+        <Card.Text className={classes.head}>
+            <p>{props.amount} WEI</p>
+        </Card.Text >
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PayCard;
